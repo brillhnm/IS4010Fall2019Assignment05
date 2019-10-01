2afe8ca6be4183d7248b5a6b1b14722ec2c8ac0e
brillhnm <brillhnm@LCBLAB2110-01.uccob.uc.edu>
2019-10-01 09:06:37 -0400

    Methods with Football

--- IS4010Fall2019Assignment05/src/footballFun/FootballFun.java
+++ IS4010Fall2019Assignment05/src/footballFun/FootballFun.java
@@ -1,9 +1,12 @@
 /*
- * IS4010 Fal 2019
  * Assignment 05
- * Bill Nicholson
- * nicholdw@ucmail.uc.edu
+ * Nina Brillhart
+ * Description: Football Methods
+ * Due Date: Resubmitted 10/3/19 
+ * Course: IS 4010
+ * Email: brillhnm@mail.uc.edu
  */
+
 package footballFun;
 
 public class FootballFun {
@@ -25,7 +28,23 @@ public class FootballFun {
 			System.out.println("At least one test failed");
 		}
 	}
+	/**
+	 * Describe the football score passed to the method.						//Java Docs
+	 * @param score The score from the game
+	 * @return the description of the score
+	 */
 	public static String translateScore (int score) {
-		return "";
+		
+			if (score ==2) return ("safety");								//if score passed to method is 2 then prints "safety"
+			if (score ==3) return ("field goal");							//if score is 3 then prints "field goal"
+			if (score == 6) return ("touchdown");							//if score is 6 then prints "touchdown"
+			if (score == 7) return ("touchdown and extra point");			//if score is 7 then prints "touchdown and extra point"
+			if (score == 8) return ("touchdown and 2-point conversion");	//if score is 8 then prints "touchdown and 2-point conversion"
+			if (score == -1) return ("");									//if score is -1 then prints "" aka nothing
+			if (score == 1) return ("invalid");								//if score is 1 then prints "invalid "
+			if (score == 10) return ("you must be playing Quidditch ");		//if score is 10 then prints "you must be playing Quidditch "
+		
+			
+		return "";															//if none of the stipulations above are true, then it returns nothing
 	}
 }
